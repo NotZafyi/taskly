0df716c9faced376f7cc8e59ddfc282d669e6494
NotZafyi <thezafyi@gmail.com>
2025-02-28 23:46:07 +0500

    changes done

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,16 +1,19 @@
-import NextAuth from 'next-auth'
-import GoogleProvider from 'next-auth/providers/google'
-import GithubProvider from "next-auth/providers/github"
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
 
-
- export const authOptions=NextAuth({
+export default NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-})
-
-
-export {authOptions as POST, authOptions as GET}
+  secret: process.env.NEXTAUTH_SECRET,
+  debug: true, // Enable debug mode
+  callbacks: {
+    async signIn(user, account, profile) {
+      console.log("SIGN IN ATTEMPT:", user, account, profile);
+      return true;
+    },
+  },
+});
