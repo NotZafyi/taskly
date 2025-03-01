555a38883b3a735d15475423c0c8850d5cdd90d2
NotZafyi <thezafyi@gmail.com>
2025-03-01 23:05:39 +0500

    changes done

--- app/components/tasks.js
+++ app/components/tasks.js
@@ -402,8 +402,8 @@ const tasks = () => {
             />
           </div>
         </div>
-        <div className="min-w-screen mt-16 ml-52 min-h-[80vh] flex flex-col gap-2">
-          <div className="hide-scrollbar min-h-full overflow-y-scroll min-w-[700] flex flex-col gap-4 p-4 bg-[#070707ba] border border-gray-900 rounded-xl">
+        <div className="min-w-[700] mt-16 ml-52 min-h-[80vh] flex flex-col  gap-2">
+          <div className="hide-scrollbar min-h-full min-w-[700] overflow-y-scroll flex flex-col gap-4 p-4 bg-[#070707ba] border border-gray-900 rounded-xl">
             <div className="flex justify-between items-center max-h-full">
               <h1 className="text-2xl p-2">Tasks</h1>
               <button
@@ -590,9 +590,9 @@ const tasks = () => {
               return (
                 <div
                   key={collectiontasks._id}
-                  className="hide-scrollbar min-h-[80vh] max-h-full overflow-y-scroll flex flex-col gap-4 min-w-full p-4 bg-[#070707ba] border border-gray-900 rounded-xl"
+                  className="hide-scrollbar min-h-[80vh] max-h-full overflow-y-scroll flex flex-col gap-4 min-w-[700]  p-4 bg-[#070707ba] border border-gray-900 rounded-xl"
                 >
-                  <div className="flex justify-between items-center max-h-full">
+                  <div className="flex justify-between min-w-[700]  items-center max-h-full">
                     <h1 className="text-2xl p-2">
                       {collectiontasks.collectionName}
                     </h1>
