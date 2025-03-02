19cb0f71e7600e073db6857f40ba2ec74a2f9a35
NotZafyi <thezafyi@gmail.com>
2025-03-02 06:13:30 +0500

    changes done

--- app/components/tasks.js
+++ app/components/tasks.js
@@ -412,7 +412,7 @@ const tasks = () => {
 
 
           <div className="box hide-scrollbar min-h-full  overflow-y-scroll flex flex-col gap-4 p-4 bg-[#070707ba] border border-gray-900 rounded-xl">
-            <div className="box flex justify-between items-center max-h-full">
+            <div className="flex justify-between items-center max-h-full">
               <h1 className="text-2xl p-2">Tasks</h1>
               <button
                 onClick={() => setisTaskAddModalOpen(true)}
@@ -600,7 +600,7 @@ const tasks = () => {
                   key={collectiontasks._id}
                   className="box hide-scrollbar min-h-full overflow-y-scroll flex flex-col gap-4 p-4 bg-[#070707ba] border border-gray-900 rounded-xl"
                 >
-                  <div className="box flex justify-between items-center max-h-full">
+                  <div className="flex justify-between items-center max-h-full">
                     <h1 className="text-2xl p-2">
                       {collectiontasks.collectionName}
                     </h1>
