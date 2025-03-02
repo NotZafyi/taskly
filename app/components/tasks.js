47874175fae1e23761cef80614b901c9dd418e3d
NotZafyi <thezafyi@gmail.com>
2025-03-02 06:00:38 +0500

    changes done

--- app/components/tasks.js
+++ app/components/tasks.js
@@ -593,9 +593,9 @@ const tasks = () => {
               return (
                 <div
                   key={collectiontasks._id}
-                  className="box hide-scrollbar min-h-[80vh] max-h-full overflow-y-scroll flex flex-col gap-4  p-4 bg-[#070707ba] border border-gray-900 rounded-xl"
+                  className="box hide-scrollbar min-h-full overflow-y-scroll flex flex-col gap-4 p-4 bg-[#070707ba] border border-gray-900 rounded-xl"
                 >
-                  <div className="box flex justify-between  items-center max-h-full">
+                  <div className="box flex justify-between items-center max-h-full">
                     <h1 className="text-2xl p-2">
                       {collectiontasks.collectionName}
                     </h1>
